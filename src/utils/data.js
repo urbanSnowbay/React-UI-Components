eef0a80c7cb3725ec9ae35c6537e5e94c76c2905
Haiqal Mochammad Satria Utama <haiqal231@gmail.com>
2023-09-20 14:32:31 +0700

    initial commit

---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+// file ini merupakan langkah awal untuk menyiapkan data kontak yang hendak ditampilkan 
+
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Dimas Saputra',
+            tag: 'dimasmds',
+            imageUrl: '/images/dimasmds.jpeg',
+        },
+        {
+            id: 2,
+            name: 'Arif Faizin',
+            tag: 'arifaizin',
+            imageUrl: '/images/arifaizin.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Rahmat Fajri',
+            tag: 'rfajri27',
+            imageUrl: '/images/rfajri27.jpeg',
+        },
+    ];
+}
+
+export { getData };
